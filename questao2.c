c41fe946a6133fb6b44f1b3751897a00d9f756c5
Mayferreira31 <mdsf.gen21@uea.edu.br>
2022-08-28 05:07:42 -0300

    Questão da Mayara

---
+++ questao2.c
@@ -0,0 +1,28 @@
+/*  Jean Victor Lopes Mendes - 2115090011
+    Mayara da Silva Ferreira -2115200063
+    Rafael Lucas  Gaspar da Cruz - 1615100348*/
+
+
+#include<stdio.h>
+
+int main(int argc,char**argv)
+{
+        int a;
+
+        printf("Digite um número:\n");
+        scanf("%d",&a);
+
+        if(a>0)
+        {
+
+        printf("O número %d é positivo",a);
+        }
+
+        else
+        {
+
+        printf("O número %d é negativo",a);
+        }
+
+        return 0;
+}
